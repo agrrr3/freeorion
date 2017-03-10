697c0a8253c800ece935cc0afdfc5c552211b854
O01eg <o01eg@yandex.ru>
2017-03-10 23:40:07 +0300

    Clean up Message named ctors arguments.

--- UI/PlayerListWnd.cpp
+++ UI/PlayerListWnd.cpp
@@ -206,7 +206,7 @@ namespace {
             case Networking::CLIENT_TYPE_HUMAN_MODERATOR:   ModeratorIcon()->OrthoBlit(UpperLeft() + m_player_type_icon_ul, UpperLeft() + m_player_type_icon_ul + ICON_SIZE); break;
             default:    break;
             }
- 
+
             if (m_host)
                 HostIcon()->OrthoBlit(UpperLeft() + m_host_icon_ul, UpperLeft() + m_host_icon_ul + ICON_SIZE);
 
@@ -812,45 +812,37 @@ void PlayerListWnd::PlayerRightClicked(GG::ListBox::iterator it, const GG::Pt& p
     if (popup.Run()) {
         switch (popup.MenuID()) {
         case 1: {   // WAR_DECLARATION
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             WarDeclarationDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(WarDeclarationDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
         case 2: {   // PEACE_PROPOSAL
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             PeaceProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(PeaceProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
         case 3: {   // PEACE_ACCEPT
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             AcceptPeaceDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(AcceptPeaceDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
 
         case 6: {   // ALLIES_PROPOSAL
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             AlliesProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(AlliesProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
         case 7: {   // ALLIES_ACCEPT
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             AcceptAlliesDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(AcceptAlliesDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
         case 8: {   // END_ALLIANCE_DECLARATION
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             EndAllianceDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(EndAllianceDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
 
         case 4: {   // PROPOSAL_CANCEL
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             CancelDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(CancelDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
         case 9: {   // PROPOSAL_REJECT
-            net.SendMessage(DiplomacyMessage(client_player_id, clicked_player_id,
-                                             RejectProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
+            net.SendMessage(DiplomacyMessage(RejectProposalDiplomaticMessage(client_empire_id, clicked_empire_id)));
             break;
         }
 
