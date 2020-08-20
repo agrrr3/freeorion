4ab4ebe1c90506e54456c8ed91f99295a2407e04
Axel Groß <axel.gross@iaeste.at>
2020-08-20 18:38:35 +0200

    Review fixes 03 - primary_expr.alias() -> primary_expr

--- parse/IntValueRefParser.cpp
+++ parse/IntValueRefParser.cpp
@@ -141,7 +141,7 @@ parse::int_arithmetic_rules::int_arithmetic_rules(
     named_int_valueref
         = (     tok.Named_ >> tok.Integer_
              >  label(tok.Name_) > tok.string
-             >  label(tok.Value_) > primary_expr.alias()
+             >  label(tok.Value_) > primary_expr
           ) [
              // Register the value ref under the given name by lazy invoking RegisterValueRef
              parse::detail::open_and_register_as_string_(_2, _3, _pass),
