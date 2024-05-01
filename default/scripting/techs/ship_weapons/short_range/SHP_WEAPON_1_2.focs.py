96953da2339eb897a91b155f5755223febe92032
agrrr3 <3252041+agrrr3@users.noreply.github.com>
2024-05-01 20:23:07 +0200

    WIP Use ShipPartMeter in focs.py - sitrep with Mass Driver 2

--- default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_1_2.focs.py
+++ default/scripting/techs/ship_weapons/short_range/SHP_WEAPON_1_2.focs.py
@@ -1,6 +1,6 @@
 from focs._tech import *
 from macros.base_prod import TECH_COST_MULTIPLIER
-from techs.ship_weapons.ship_weapons import WEAPON_UPGRADE_CAPACITY_EFFECTS
+from techs.ship_weapons.ship_weapons import WEAPON_UPGRADE_SECONDARY_STAT_EFFECTS
 
 Tech(
     name="SHP_WEAPON_1_2",
@@ -11,6 +11,6 @@ Tech(
     researchturns=2,
     tags=["PEDIA_SR_WEAPON_TECHS"],
     prerequisites=["SHP_ROOT_AGGRESSION"],
-    effectsgroups=WEAPON_UPGRADE_CAPACITY_EFFECTS("SHP_WEAPON_1_2", "SR_WEAPON_1_1", 1),
+    effectsgroups=WEAPON_UPGRADE_SECONDARY_STAT_EFFECTS("SHP_WEAPON_1_2", "SR_WEAPON_1_1", 1),
     graphic="icons/ship_parts/mass-driver-2.png",
 )
