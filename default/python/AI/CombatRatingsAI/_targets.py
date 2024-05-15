563df6e3f60dab4a12f55376f7790da8ed455f92
agrrr3 <agrrr3@users.noreply.github.com>
2024-05-15 14:40:49 +0200

    WIP AI add multi target class heuristics

--- default/python/AI/CombatRatingsAI/_targets.py
+++ default/python/AI/CombatRatingsAI/_targets.py
@@ -17,3 +17,21 @@ def get_allowed_targets(partname: str) -> int:
             )
             _issued_errors.add(partname)
         return CombatTarget.ANY
+
+def get_multi_target_split_damage_factor(allowed_targets: int, target_class: int) -> float:
+    """
+    Return a heuristic factor how much expected damage needs to be scaled down
+    for a certain each class of targets in case there multiple valid target classes.
+    If the military AI puts the ship to the correct use, the expected damage will be higher
+    than a simple division by the number of classes.
+    """
+    target_classes_cnt = 0
+    # TODO the expected number of targets is usually fighters > ships > planets, so e.g. planets should not distract much from other targets; so probably the result should depend on a certain target class
+    #target_classes_cnt += int (allowed_targets & AIDependencies.CombatTarget.FIGHTER) # point defense is not valued in the ship designer yet
+    target_classes_cnt += int (allowed_targets & AIDependencies.CombatTarget.PLANET)
+    target_classes_cnt += int (allowed_targets & AIDependencies.CombatTarget.SHIP)
+    if target_classes_cnt == 2:
+        return 0.7
+    if target_classes_cnt == 3:
+        return 0.5
+    return 1.0
