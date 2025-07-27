ac43bd0fc41ec22805b076d407d4f9bba0777221
agrrr3 <agrrr3@users.noreply.github.com>
2025-07-27 15:52:29 +0200

    [WIP][dontmerge][depends] OUTPOST - debug stuff, all this depends on the movetobranch

--- default/scripting/techs/construction/OUTPOST.focs.py
+++ default/scripting/techs/construction/OUTPOST.focs.py
@@ -1,13 +1,16 @@
 from focs._effects import (
     Abs,
+    Described,
     EffectsGroup,
     EmpireHasAdoptedPolicy,
+    GenerateSitRepMessage,
     HasEmpireStockpile,
     HasSpecies,
     IsSource,
     IsTarget,
     MaxOf,
     MinOf,
+    NoConditionLog,
     NumPoliciesAdopted,
     OwnedBy,
     Planet,
@@ -18,6 +21,7 @@ from focs._effects import (
     SetHappiness,
     SetMaxTroops,
     SetTargetConstruction,
+    Ship,
     Source,
     StatisticIf,
     Target,
@@ -26,10 +30,21 @@ from focs._effects import (
 from focs._tech import *
 from macros.priorities import (
     AFTER_ALL_TARGET_MAX_METERS_PRIORITY,
+    BEFORE_ANYTHING_ELSE_PRIORITY,
     METER_OVERRIDE_PRIORITY,
     TARGET_AFTER_2ND_SCALING_PRIORITY,
 )
 
+#connecty =                                            ResourceSupplyConnected(empire=Source.Owner,condition=Target.SystemID==LocalCandidate.SystemID)
+#connectyy = Planet() & OwnedBy(empire=Source.Owner) & ResourceSupplyConnected(empire=Source.Owner,condition=Ship & InSystem(id=Target.SystemID))
+
+# in MoveTo effect
+connecty =                                            ResourceSupplyConnected(empire=Source.Owner,condition=IsTarget)
+# 
+# 3 connectyy = System & Contains(Planet() & OwnedBy(empire=Source.Owner)) & ResourceSupplyConnected(empire=Source.Owner,condition=IsTarget)
+connectyy = System & Contains(Planet() & OwnedBy(empire=Source.Owner)) & ResourceSupplyConnected(empire=Source.Owner,condition=IsTarget)
+
+# Object(id=Source.SystemID)
 Tech(
     name="CON_OUTPOST",
     description="CON_OUTPOST_DESC",
@@ -41,6 +56,44 @@ Tech(
     unlock=Item(type=UnlockBuilding, name="BLD_ABANDON_OUTPOST"),
     # Effects for outposts
     effectsgroups=[
+        # PROTOTYPE move newly built ships in supply group
+        # maybe this needs two cases; mixed fleets and fleets which only contain newly built ships
+        # but we dont care and hope the backend takes care of empty fleets
+        EffectsGroup(
+            scope=Ship
+            & OwnedBy(empire=Source.Owner)
+            & (LocalCandidate.Age==1),
+            priority=BEFORE_ANYTHING_ELSE_PRIORITY,
+            effects=[
+                MoveTo(
+#                    destination=NumberOf(number=1, condition=Fleet & ~Contains(LocalCandidate.Age==1) & NoConditionLog & OwnedBy(empire=Source.Owner) & NoConditionLog & connecty & NoConditionLog)
+                    destination=NoConditionLog & NumberOf(number=1, condition=System
+                        & Contains(Planet() & OwnedBy(empire=Source.Owner))
+                        & NoConditionLog
+                        & connecty
+                        & NoConditionLog
+                    )
+                ),
+                GenerateSitRepMessage(  # and tell the owner it happened                                                                                                                                                                   
+                    message="EFFECT send %ship% from %planet%  age: %rawtext%   targets: %rawtext:targets%  maybe: %system:maybe%",
+                    label="Custom_1",
+                    stringtablelookup=False,
+                    icon="icons/tech/categories/spy.png",
+                    parameters={
+                        "planet":Source.ID,
+                        "ship":Target.ID,
+                        "rawtext":Target.Age,
+                        "targets":StatisticCount(
+                            int,
+                            condition=connectyy
+                        ),
+                        "maybe":Statistic(int, Mode, value=LocalCandidate.SystemID, condition=NumberOf(number=1, condition=connectyy)),
+                    },
+                    empire=Target.Owner,
+                ),
+
+            ]
+        ),
         # Outposts only have 50% of troops
         EffectsGroup(
             scope=Planet()
