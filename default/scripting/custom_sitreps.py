a0220984de96465719fbcb23a393ec46e5f3ab83
agrrr3 <agrrr3@users.noreply.github.com>
2026-01-06 19:36:59 +0100

    [focs][dev] Add optional custom message about adopted policies. Missing: to-string for ValueRefVectorString
    == TODO ==
    
     Custom message cant print policy NAMES - need to register to-string for ValueRefVectorString
    
    no.. produce ..rvalue..std::string_.. from this .. ValueRefVectorString
    
    no registered converter was able to produce a C++ rvalue of type std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >
    from this Python object of type ValueRefVectorString

--- default/scripting/custom_sitreps.py
+++ default/scripting/custom_sitreps.py
@@ -7,6 +7,7 @@ from focs._effects import (
     ContentFocus,
     CurrentTurn,
     EffectsGroup,
+    EmpireAdoptedPolicies,
     EmpireMeterValue,
     EnemyOf,
     Enqueued,
@@ -19,6 +20,7 @@ from focs._effects import (
     LocalCandidate,
     Location,
     Number,
+    NumPoliciesAdopted,
     OneOf,
     OwnedBy,
     OwnerHasTech,
@@ -33,6 +35,7 @@ from focs._effects import (
     TurnTechResearched,
     Unowned,
     UserString,
+    VectorCount,
     VisibleToEmpire,
 )
 from macros.priorities import END_CLEANUP_PRIORITY
@@ -56,6 +59,39 @@ debug_starlane_travel = EffectsGroup(
         parameters={"ship": Target.ID, "prev": Target.Fleet.PreviousSystemID, "next": Target.Fleet.NextSystemID},
         empire=Source.Owner,
     ),
+)
+
+# This is for exemplifying vector / list types
+debug_empire_adopted_policies = EffectsGroup(
+    scope=IsSource,
+    activation=IsSource,
+    stackinggroup="DEBUG_EMPIRE_ADOPTED_POLICIES",
+    effects=[
+        GenerateSitRepMessage(
+            message="debug_empire_adopted_policies(num): Number of currently adopted policies %rawtext:policies%",
+            label="SITREP_WELCOME_LABEL",
+            NoStringtableLookup=True,
+            icon="icons/tech/categories/spy.png",
+            parameters={"policies": NumPoliciesAdopted(empire=Source.Owner),},
+            empire=Source.Owner,
+        ),
+        GenerateSitRepMessage(
+            message="debug_empire_adopted_policies(vec_count): currently adopted policies %rawtext:policies%",
+            label="SITREP_WELCOME_LABEL",
+            NoStringtableLookup=True,
+            icon="icons/tech/categories/spy.png",
+            parameters={"policies": VectorCount(int, EmpireAdoptedPolicies(empire=Source.Owner)),},
+            empire=Source.Owner,
+        ),
+        #GenerateSitRepMessage(
+        #    message="debug_empire_adopted_policies(names): currently adopted policies %policies%",
+        #    label="SITREP_WELCOME_LABEL",
+        #    NoStringtableLookup=True,
+        #    icon="icons/tech/categories/spy.png",
+        #    parameters={"policies": EmpireAdoptedPolicies(empire=Source.Owner)},
+        #    empire=Source.Owner,
+        #),
+    ],
 )
 
 
