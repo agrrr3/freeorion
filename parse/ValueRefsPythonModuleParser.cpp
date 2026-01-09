aa91d7c3365f00b7bde21ea593dd14a821787376
agrrr3 <agrrr3@users.noreply.github.com>
2026-01-09 23:44:19 +0100

    [broken][py][pyi] try to move new vrefs to valueref module
    tries to load as a module
    
    Couldn't find file for module spec focs._value_refs.NumPartClassesInShipDesign
    23:40:34.939146 {0x00007574067fd6c0} [error] client : PythonParser.cpp:502 : Couldn't find file for module spec focs._value_refs.NumPartClassesInShipDesign
    23:40:34.939249 {0x00007574067fd6c0} [error] client : PythonParser.cpp:70 : Failed to eval
    23:40:34.942464 {0x00007574067fd6c0} [error] client : PythonCommon.cpp:151 : Traceback (most recent call last):
    23:40:34.942499 {0x00007574067fd6c0} [error] client : PythonCommon.cpp:151 :   File "/home/freeorion/build/default/scripting/macros/named_values.focs.py", line 7, in <module>
        from focs._value_refs import NumPartClassesInShipDesign,PartOfClassInShipDesign

--- parse/ValueRefsPythonModuleParser.cpp
+++ parse/ValueRefsPythonModuleParser.cpp
@@ -49,7 +49,75 @@ namespace {
             nullptr
         )); 
     }
+
+  value_ref_wrapper<int> insert_complex_i1(std::string&& vname, const boost::python::tuple& args, const boost::python::dict& kw, const std::string&& keyint1) {
+        std::unique_ptr<ValueRef::ValueRef<int>> int1;
+        auto int1_args = boost::python::extract<value_ref_wrapper<int>>(kw[keyint1]);
+        if (int1_args.check()) {
+            int1 = ValueRef::CloneUnique(int1_args().value_ref);
+        } else {
+            int1 = std::make_unique<ValueRef::Constant<int>>(boost::python::extract<int>(kw["keyint1"])());
+        }
+
+        return value_ref_wrapper<int>(std::make_shared<ValueRef::ComplexVariable<int>>(
+            std::move(vname),
+            std::move(int1),
+            nullptr,
+            nullptr,
+            nullptr,
+            nullptr
+        ));
+    }
+
+    value_ref_wrapper<int> insert_complex_i1_s1(std::string&& vname, const boost::python::tuple& args, const boost::python::dict& kw, const std::string&& keyint1, const std::string&& keystr1) {
+        std::unique_ptr<ValueRef::ValueRef<int>> int1;
+        auto int1_args = boost::python::extract<value_ref_wrapper<int>>(kw[keyint1]);
+        if (int1_args.check()) {
+            int1 = ValueRef::CloneUnique(int1_args().value_ref);
+        } else {
+            int1 = std::make_unique<ValueRef::Constant<int>>(boost::python::extract<int>(kw["keyint1"])());
+        }
+
+        std::unique_ptr<ValueRef::ValueRef<std::string>> str1;
+        if (kw.has_key(keystr1)) {
+            auto str1_args = boost::python::extract<value_ref_wrapper<std::string>>(kw[keystr1]);
+            if (str1_args.check()) {
+                str1 = ValueRef::CloneUnique(str1_args().value_ref);
+            } else {
+                str1 = std::make_unique<ValueRef::Constant<std::string>>(boost::python::extract<std::string>(kw[keystr1])());
+            }
+        }
+
+        return value_ref_wrapper<int>(std::make_shared<ValueRef::ComplexVariable<int>>(
+            std::move(vname),
+            std::move(int1),
+            nullptr,
+            nullptr,
+            std::move(str1),
+            nullptr
+        ));
+    }
 }
+
+value_ref_wrapper<int> insert_num_part_classes_in_ship_design_(const boost::python::tuple& args, const boost::python::dict& kw) {
+         std::unique_ptr<ValueRef::ValueRef<int>> design;
+         auto design_args = boost::python::extract<value_ref_wrapper<int>>(kw["design"]);
+         if (design_args.check()) {
+             design = ValueRef::CloneUnique(design_args().value_ref);
+         } else {
+             design = std::make_unique<ValueRef::Constant<int>>(boost::python::extract<int>(kw["design"])());
+         }
+
+         return value_ref_wrapper<int>(std::make_shared<ValueRef::ComplexVariable<int>>(
+             "NumPartClassesInShipDesign",
+             std::move(design),
+             nullptr,
+             nullptr,
+             nullptr,
+             nullptr
+         ));
+}
+
 
 BOOST_PYTHON_MODULE(_value_refs) {
     boost::python::docstring_options doc_options(true, true, false);
@@ -66,5 +134,14 @@ BOOST_PYTHON_MODULE(_value_refs) {
             const auto f_insert_ship_designs = [name](const boost::python::tuple& args, const boost::python::dict& kw) { return insert_ship_designs(name, args, kw); };
             boost::python::def(name.data(), boost::python::raw_function(f_insert_ship_designs));
     }
+
+    const auto f_insert_num_part_classes_in_ship_design_ = [](const boost::python::tuple& args, const boost::python::dict& kw) { return insert_complex_i1("NumPartClassesInShipDesign", args, kw, "design"); };
+    //boost::python::def("NumPartClassesInShipDesign", boost::python::raw_function(f_insert_num_part_classes_in_ship_design_));
+    boost::python::def("NumPartClassesInShipDesign", boost::python::raw_function(insert_num_part_classes_in_ship_design_));
+    
+    const auto f_insert_part_of_class_in_ship_design_ = [](const boost::python::tuple& args, const boost::python::dict& kw) { return insert_complex_i1_s1("PartOfClassInShipDesign", args, kw, "design", "name"); };
+    boost::python::def("PartOfClassInShipDesign", boost::python::raw_function(f_insert_part_of_class_in_ship_design_));
+
+
 }
 
