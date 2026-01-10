da0188ad0dbb0ddc5f2cc3f29b894837c0c295c0
agrrr3 <agrrr3@users.noreply.github.com>
2026-01-10 08:50:39 +0100

    review-01 changes: some FlexibleToString support for vector types

--- universe/ValueRef.h
+++ universe/ValueRef.h
@@ -218,13 +218,7 @@ decltype(auto) FlexibleToString(T&& t)
     } else if constexpr (std::is_same_v<std::decay_t<T>, std::string_view>) {
         return std::string{t};
 
-    } else if constexpr (std::is_same_v<T, std::vector<int>>) {
-        std::string retval;
-        for (auto& ts: t)
-            retval.append(std::to_string(ts));
-        return retval;
-
-    } else if constexpr (std::is_same_v<T, std::vector<double>>) {
+    } else if constexpr (std::is_same_v<T, std::vector<int>> || std::is_same_v<T, std::vector<double>>) {
         std::string retval;
         for (auto& ts: t)
             retval.append(std::to_string(ts));
