24d71a9a0cd0a4d11f35b321a9c81d32923b0fa0
agrrr3 <agrrr3@users.noreply.github.com>
2026-01-31 21:18:24 +0100

    Try fix to_string from ShipPartClass enum

--- universe/ValueRefs.cpp
+++ universe/ValueRefs.cpp
@@ -322,6 +322,7 @@ std::string FlexibleToString(StarType t) { return EnumToString(t); }
 std::string FlexibleToString(PlanetEnvironment t) { return EnumToString(t); }
 std::string FlexibleToString(PlanetType t) { return EnumToString(t); }
 std::string FlexibleToString(PlanetSize t) { return EnumToString(t); }
+std::string FlexibleToString(ShipPartClass t) { return EnumToString(t); }
 std::string FlexibleToString(Visibility t) { return EnumToString(t); }
 std::string FlexibleToString(UniverseObjectType t) { return EnumToString(t); }
 
