e50fa3f410cd32d791565197691c8965b470d75f
agrrr3 <agrrr3@users.noreply.github.com>
2026-03-20 15:49:46 +0100

    Restrict planetary starlane drive to three jumps per planet

--- default/scripting/species/species_macros/advanced_focus.py
+++ default/scripting/species/species_macros/advanced_focus.py
@@ -11,12 +11,14 @@ from focs._effects import (
     Fleet,
     Focus,
     GenerateSitRepMessage,
+    HasSpecial,
     InSystem,
     IsBuilding,
     IsSource,
     LocalCandidate,
     MoveTo,
     MoveTowards,
+    NamedIntegerLookup,
     Number,
     Object,
     OwnedBy,
@@ -29,6 +31,7 @@ from focs._effects import (
     SetTargetPopulation,
     Ship,
     Source,
+    SpecialCapacity,
     Stationary,
     System,
     Target,
@@ -47,6 +50,11 @@ from macros.priorities import (
 PLANETARY_DRIVE_ACTIVATION = (
     Planet()
     & Focus(type=["FOCUS_PLANET_DRIVE"])
+    # TODO: add low / high to has special parsing
+    & (
+        ~HasSpecial(name="STARLANE_DRIVE_INSTABILITY_SPECIAL")
+        | (SpecialCapacity(name="STARLANE_DRIVE_INSTABILITY_SPECIAL", object=Source.ID) < NamedIntegerLookup(name="STARLANE_DRIVE_MAX_JUMPS"))
+    )
     & WithinStarlaneJumps(
         jumps=1,
         condition=System
@@ -116,6 +124,7 @@ ADVANCED_FOCUS_EFFECTS = [
                 )
                 & ~Contains(IsSource)
             ),
+            SetSpecialCapacity(name="STARLANE_DRIVE_INSTABILITY_SPECIAL", capacity=Value + 1),
             GenerateSitRepMessage(
                 message="EFFECT_PLANET_DRIVE",
                 label="EFFECT_PLANET_DRIVE_LABEL",
